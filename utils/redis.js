e8fd528ca5315dae796782231469b1f6a0fd6cc2
Mythamor <bethmithamo.bm@gmail.com>
2024-02-12 19:15:14 +0300

    Fix lint errors

--- utils/redis.js
+++ utils/redis.js
@@ -1,40 +1,40 @@
 import { createClient } from 'redis';
 
-
 class RedisClient {
   constructor() {
-    this.client = createClient();
+    this.client = createClient({ debug_mode: true });
 
     // Handle Refis client errors
-    this.client.on('error', err => console.error('Redis Client Error', err));
- }
- 
+    this.client.on('error', (err) => console.error('Redis Client Error', err));
+    this.client.connect();
+  }
+
   isAlive() {
     return this.client.connected;
   }
- 
+
   async get(key) {
     return new Promise((resolve, reject) => {
       this.client.get(key, (err, reply) => {
         if (err) {
           console.error('Redis GET Error:', err);
-	  reject(err);
-	} else {
-	   resolve(reply);
-      }
+          reject(err);
+        } else {
+          resolve(reply);
+        }
+      });
     });
-  });
-}
-  
+  }
+
   async set(key, value, duration) {
     return new Promise((resolve, reject) => {
       this.client.set(key, value, 'EX', duration, (err, reply) => {
         if (err) {
           console.error('Redis SET Error:', err);
-	  reject(err);
-	} else {
-	   resolve(reply);
-	}
+          reject(err);
+        } else {
+          resolve(reply);
+        }
       });
     });
   }
@@ -44,11 +44,11 @@ class RedisClient {
       this.client.del(key, (err, reply) => {
         if (err) {
           console.error('Redis DEL Error:', err);
-	} else {
-            resolve(reply);
-	}
+          reject(err);
+        } else {
+          resolve(reply);
+        }
       });
-
     });
   }
 }
@@ -56,4 +56,4 @@ class RedisClient {
 // Create an instance of RedisClient n export it
 const redisClient = new RedisClient();
 
-export default redisClient;
+module.exports = redisClient;
