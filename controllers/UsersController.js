31919a072d3b7aff79205dd0fcd7db7ee22c74b7
Mythamor <bethmithamo.bm@gmail.com>
2024-02-13 18:19:34 +0300

    Fixing errors

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -15,36 +15,36 @@ export const getAuthHeader = (req) => {
 
 class UsersController {
   static async postNew(req, res) {
-      const { email, password } = req.body;
+    const { email, password } = req.body;
 
-      // Check if email and password are provided
-      if (!email) {
-        res.status(400).json({ error: 'Missing email' });
-	res.end();
-	return;
-      }
-      if (!password) {
-        res.status(400).json({ error: 'Missing password' });
-	res.end();
-	return;
-      }
-
-      // Check if the email already exists
-      const existingUser = await dbClient.userExist(email);
-      if (existingUser) {
-        res.status(400).json({ error: 'Already exist ' });
-	res.end();
-	return;
-      }
-
-      // Create a new user obj
-      const newUser = await dbClient.createUser(email, password);
-      const userId = `${newUser.insertedId}`;
-
-      // Return the new user with email and id
-      return res.status(201).json({ userId, email });
+    // Check if email and password are provided
+    if (!email) {
+      res.status(400).json({ error: 'Missing email' });
       res.end();
+      return;
+    }
+    if (!password) {
+      res.status(400).json({ error: 'Missing password' });
+      res.end();
+      return;
+    }
+
+    // Check if the email already exists
+    const existingUser = await dbClient.userExist(email);
+    if (existingUser) {
+      res.status(400).json({ error: 'Already exist ' });
+      res.end();
+      return;
+    }
+
+    // Create a new user obj
+    const newUser = await dbClient.createUser(email, password);
+    const userId = `${newUser.insertedId}`;
+
+    // Return the new user with email and id
+    res.status(201).json({ userId, email });
+    res.end();
   }
-};
+}
 
 module.exports = UsersController;
